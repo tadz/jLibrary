3c87ca792d4d617e825336205736963e5c47d831
zbikowa <agnieszka.jasiu@gmail.com>
2015-12-11 15:20:40 +0100

    edycja rekordu

--- src/model/AplikacjaModel.java
+++ src/model/AplikacjaModel.java
@@ -6,25 +6,25 @@ import java.sql.*;
 public class AplikacjaModel {
 
 	public static Vector<Book> findAll() {
+		Vector<Book> listBooks = new Vector<Book>();
+		
 		try {
-			Vector<Book> listBooks = new Vector<Book>();
-
 			PreparedStatement ps = DB.getConnection().prepareStatement(
 					"select * from BOOKSTORE");
-
+			
 			ResultSet rs = ps.executeQuery();
-
+			
 			while (rs.next()) {
 				Book b = new Book(rs.getInt("id"), rs.getString("title"),
 						rs.getString("author"), rs.getString("publisher"),
-						rs.getInt("publicationYear"), rs.getString("owner"));
-
+						rs.getInt("publicationYear"),rs.getString("isbn"), rs.getString("owner"));
+				
 				listBooks.add(b);
 			}
 
 			return listBooks;
 		} catch (Exception e) {
-			return null;
+			return listBooks;
 		}
 	}
 }
